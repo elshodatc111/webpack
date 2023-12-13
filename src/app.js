f44b3dd6c84728e5c0a32c42e20ab4a907d6b533
Elshod Musurmonov <elshodatc1116@gmail.com>
2023-12-13 22:46:33 +0500

    Loader CSS ulash

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-import getData from "./request";
-import { hello, number } from "./alert";
+import './css/style.css'
+
+import getData from "./js/request"
+
+import { hello, number } from "./js/alert"
+
 getData("Elshod")
 
 console.log(number)
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,15 @@ module.exports = {
     output: {
         path:path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
-    }
+    },
     // plagins
     // Loaders
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
